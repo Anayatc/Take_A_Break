285857a28a909b77da5d7536a82a602872edb2f5
anayatc@gmail.com <anayatc@sky.com>
2017-05-15 22:11:55 +0100

    initial skeleton

--- Break.py
+++ Break.py
@@ -1,3 +1,10 @@
 import webbrowser
+import time
 
-webbrowser.open('https://www.youtube.com/watch?v=fBdRI-1mmX4')
+
+def take_break():
+    total_breaks = 3
+    count = 0
+    while count < total_breaks:
+        webbrowser.open('https://www.youtube.com/watch?v=fBdRI-1mmX4')
+        time.sleep(10)
