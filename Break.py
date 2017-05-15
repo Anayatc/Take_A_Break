2e2b5498f24014dde4c7ab0c621707ddd23a06f1
anayatc@gmail.com <anayatc@sky.com>
2017-05-15 22:15:33 +0100

    adapted for pomodoro method

--- Break.py
+++ Break.py
@@ -3,11 +3,11 @@ import time
 
 
 def take_break():
-    total_breaks = 3
+    total_breaks = 5
     count = 0
     while count < total_breaks:
         webbrowser.open('https://www.youtube.com/watch?v=fBdRI-1mmX4')
-        time.sleep(10)
+        time.sleep(1500)
         count += 1
 
 take_break()
